7f3d3b66535f96542b076e21810aa3367909aa95
Fanteline <gaboodle1320@gmail.com>
2021-07-15 11:48:48 -0500

    added even better left and right sounds

--- dontCrash.py
+++ dontCrash.py
@@ -67,18 +67,20 @@ class Player(pygame.sprite.Sprite):
 
     def move(self):
         pressed_keys = pygame.key.get_pressed()
-        if self.rect.top > 0:
+        '''if self.rect.top > 0:
             if pressed_keys[K_UP]:
                 self.rect.move_ip(0, -5)
         if self.rect.bottom < SCREEN_HEIGHT:
             if pressed_keys[K_DOWN]:
-                self.rect.move_ip(0, 5)
+                self.rect.move_ip(0, 5)'''
         if self.rect.left > 0:
             if pressed_keys[K_LEFT]:
                 self.rect.move_ip(-5, 0)
+                pygame.mixer.Sound('left.wav').play()
         if self.rect.right < SCREEN_WIDTH:
             if pressed_keys[K_RIGHT]:
-                self.rect.move_ip(5, 0)  
+                self.rect.move_ip(5, 0)
+                pygame.mixer.Sound('right.wav').play()
 
     def vulnerable(self):
         global lastHIT
